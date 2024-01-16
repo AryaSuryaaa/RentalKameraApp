cb8b3b80f8b6599672153ef0163600511ca18f99
AryaSurya <suryasaputra914@gmail.com>
2024-01-16 21:30:28 +0700

    memperbaiki struk activity

--- app/src/main/java/com/ris/rentalku/StrukActivity.java
+++ app/src/main/java/com/ris/rentalku/StrukActivity.java
@@ -8,21 +8,21 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StrukActivity extends AppCompatActivity {
-    TextView nama_penyewa,jenis_mobil,lama_sewa,total,uang_bayar,uang_kembali;
+    TextView nama_penyewa, jenis_kamera,lama_sewa,total,uang_bayar,uang_kembali;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_struk);
         nama_penyewa = findViewById(R.id.nama_penyewa);
-        jenis_mobil = findViewById(R.id.jenis_mobil);
+        jenis_kamera = findViewById(R.id.jenis_kamera);
         lama_sewa = findViewById(R.id.lama_sewa);
         total = findViewById(R.id.total);
         uang_bayar = findViewById(R.id.uang_bayar);
         uang_kembali = findViewById(R.id.uang_kembali);
 
         nama_penyewa.setText(getIntent().getStringExtra("nama"));
-        jenis_mobil.setText(getIntent().getStringExtra("mobil"));
+        jenis_kamera.setText(getIntent().getStringExtra("kamera"));
         lama_sewa.setText(String.valueOf(getIntent().getIntExtra("lama",0)));
         total.setText(String.valueOf(getIntent().getIntExtra("total",0)));
         uang_bayar.setText(String.valueOf(getIntent().getIntExtra("uang",0)));
